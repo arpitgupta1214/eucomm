eba86ae76330a81323057cbb444d157268294150
Arpit Gupta <arpitgupta1214@gmail.com>
2021-09-24 23:00:38 +0530

    team page added

---
+++ pages/about/team.jsx
@@ -0,0 +1,57 @@
+import Layout from "components/Layouts";
+import Newsletter from "components/Newsletter";
+import Image from "next/image";
+
+const Team = (props) => {
+  return (
+    <div className="w-full mt-16 flex flex-col items-center">
+      {/* head */}
+      <div className="mb-6 text-5xl font-bold">{props.pageHead}</div>
+
+      {/* teams */}
+      <div className="w-full max-w-4xl mb-16">
+        {props.teams.map((team) => (
+          <div key={`team-${team.name}`} className="w-full mb-16">
+            <div className="mb-4 text-sm font-bold">{team.name}</div>
+            <div className="w-full grid grid-cols-4 gap-6">
+              {team.members.map((member) => (
+                <div key={`${team.name}-${member.name}`} className="w-full">
+                  {/* img */}
+                  <div className="mb-3 w-full rounded-xl overflow-hidden">
+                    <Image
+                      src={member.image}
+                      alt=""
+                      width={200}
+                      height={200}
+                      layout="responsive"
+                    />
+                  </div>
+                  <div className="mb-1 font-bold text-xl">{member.name}</div>
+                  <div className="text-xs">{member.jobTitle}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="w-full max-w-content mb-32 rounded-xl overflow-hidden">
+        <Newsletter />
+      </div>
+    </div>
+  );
+};
+
+export const getStaticProps = async () => {
+  const staticData = await import("data/about/team/data.json").then(
+    (data) => data.default
+  );
+
+  return {
+    props: {
+      ...staticData,
+    },
+  };
+};
+
+Team.layout = Layout;
+export default Team;
