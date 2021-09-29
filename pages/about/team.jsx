8d2ab8ad482c3ab5d8bb6d22fa0ad989a2447f6a
Arpit Gupta <arpitgupta1214@gmail.com>
2021-09-29 18:01:35 +0530

    meet our team page done

--- pages/about/team.jsx
+++ pages/about/team.jsx
@@ -12,29 +12,33 @@ const Team = (props) => {
       <div className="w-full max-w-4xl mb-16">
         {props.teams.map((team) => (
           <div key={`team-${team.name}`} className="w-full mb-16">
-            <div className="mb-4 text-sm font-bold">{team.name}</div>
+            <div className="mb-4 text-sm font-bold text-skin-light">
+              {team.name}
+            </div>
             <div className="w-full grid grid-cols-4 gap-6">
               {team.members.map((member) => (
                 <div key={`${team.name}-${member.name}`} className="w-full">
                   {/* img */}
-                  <div className="mb-3 w-full rounded-xl overflow-hidden">
+                  <div className="mb-3 w-full">
                     <Image
                       src={member.image}
                       alt=""
-                      width={200}
-                      height={200}
+                      width={1}
+                      height={1}
                       layout="responsive"
                     />
                   </div>
                   <div className="mb-1 font-bold text-xl">{member.name}</div>
-                  <div className="text-xs">{member.jobTitle}</div>
+                  <div className="text-xs text-skin-light">
+                    {member.jobTitle}
+                  </div>
                 </div>
               ))}
             </div>
           </div>
         ))}
       </div>
-      <div className="w-full max-w-content mb-32 rounded-xl overflow-hidden">
+      <div className="content-sm mb-32">
         <Newsletter />
       </div>
     </div>
